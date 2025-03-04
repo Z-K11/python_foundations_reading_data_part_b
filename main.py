ed22a96ef716bfa3b8790775a40953a4387c7e44
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-03-04 16:38:07 +0500

    finalized our query

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ else:
     print(f'Download Complete Saved at {file_path}')
 path='data/base_ball.db'
 con =sq.Connection(path)
+query = '''SELECT *
+FROM allstarfull;'''
+all_startobservation = pd.read_sql(query,con)
+print(all_startobservation.head())
+all_table_observations = pd.read_sql("SELECT * FROM sqlite_master",con)
+print(all_table_observations)
+best_query= '''SELECT playerID, sum(GP) AS num_games_played , AVG(startingPos) AS avg_starting_pos
+FROM allstarfull
+GROUP BY playerID
+ORDER BY num_games_played DESC, avg_starting_pos ASC
+LIMIT 3 '''
+best = pd.read_sql(best_query,con)
+print(best.head())
